a8a3ddad1fb39d5f29c7c43fbbfe7814e4dfacb0
LG <gihyeonghan@gmail.com>
2023-04-03 08:41:35 +0900

    feat(initialize commit): initialize commit
    initialize commit

---
+++ lib/res/Colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class Colors{
+  static const Color RED = Color(0x00ff0000);
+  static const Color BLUE = Color(0x000000ff);
+  static const Color GREEN = Color(0x0000ff00);
+  
+  /// Application Concept Color
+  static const Color CONCEPT_COLOR = GREEN;
+}

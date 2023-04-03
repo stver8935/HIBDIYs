2d1c5d239d8a7a1f457ee254e417e689ab602258
LG <gihyeonghan@gmail.com>
2023-04-03 17:15:07 +0900

    feat(공통 클래스 추가): 어플리케이션 내에서 사용 할 공통 문자열 클래스 추가
    어플리케이션 내에서 사용 할 공통 문자열 클래스 추가

---
+++ lib/res/Strings.dart
@@ -0,0 +1,99 @@
+class Strings{
+// Nevigation Menu
+  /** 네비게이션 메뉴 - 홈 */
+  static const String NAV_HOME = "홈";
+  /** 네비게이션 메뉴 - 장치 리스트 */
+  static const String NAV_MY_DEVICE = "내 기기";
+  /** 네비게이션 메뉴 - 판매 관리 */
+  static const String NAV_SALE_MANAGE = "판매 관리";
+  /** 네비게이션 메뉴 - 설정 */
+  static const String NAV_SETTING = "설정";
+
+
+//Sign In
+  /** 아이디 */
+  static const String ID = "아이디";
+  /** 비밀번호 */
+  static const String PASSWORD = "비밀번호";
+  /** 비밀번호 확인 */
+  static const String CONFIRM_PASSWORD = "비밀번호 확인";
+  /** 이용약관 */
+  static const String TERM_OF_SERVICE = "이용약관";
+
+
+//MyInfo List
+  /** 내정보 */
+  static const String MY_INFO = "내정보";
+  /** 장치 추가 */
+  static const String ADD_DEVICE = "장치 추가";
+  /** 장비 정보 상세 보기*/
+  static const String DEVICE_INFO_DETAIL = "장비정보 상세";
+  /** 앱 설정 */
+  static const String APP_SETTING = "앱 설정";
+  /** 자주 묻는 질문 */
+  static const String FAQ = "자주 묻는 질문";
+  /** 오픈 라이센스 */
+  static const String OPEN_LICENSE = "오픈 라이센스";
+  /** 로그인 */
+  static const String LOG_IN = "로그인";
+  /** 로그인 */
+  static const String SIGN_IN = "로그인";
+  /** 구글 로그인 */
+  static const String GOOGLE_LOGIN = "구글 로그인";
+  ///
+  static const String KAKAO_LOGIN = "카카오 로그인";
+  /** 회원 가입 */
+  static const String SIGN_UP = "회원가입";
+  /** 로그아웃 */
+  static const String LOG_OUT = "로그아웃";
+  static const String SALE_PRODUCT_MANAGE = "판매물품 관리";
+
+
+  static const String YES = "예";
+  static const String NO = "아니오";
+
+// Dialog
+  /// Confirm Dialog - 장치 추가
+  static const String DLG_CONF_ADD_DEVICE = "장치를 추가 하시겠습니까?";
+  /// Confirm Dialog - 로그아웃
+  static const String DLG_CONF_LOG_OUT = "로그아웃 하시겠습니까?";
+  ///
+  static const String DLG_CONF_ADD_PRODUCT = "물품을 등록 하시겠습니까?";
+  ///
+  static const String DLG_CONF_APLY_CHANGE = "변경된 사항을 적용 하시겠습니까?";
+  static const String DLG_CONF_EXIT_APP = "앱을 종료 하시겠습니까?";
+
+  // data
+  static const String LAST_UDT = "최종 업데이트 날짜";
+  static const String SUCCESS = "성공";
+  static const String FAIL = "실패";
+
+  static const String PLZ_INSERT_ID = "아이디를 입력해 주세요.";
+  static const String PLZ_INSERT_PWD = "비밀번호를 입력해 주세요.";
+  static const String PLZ_INSERT_EMAIL ="이메일을 입력해 주세요.";
+  static const String PLZ_INSERT_PHON_NUM ="핸드폰 번호를 입력해 주세요";
+
+
+  // FAQ LIST
+  static const String FAQ_1 = "자주 묻는 질문 1";
+  static const String FAQ_RES_1 = "자주 묻는 질문 답변 1";
+
+  static const String FAQ_2 = "자주 묻는 질문 2";
+  static const String FAQ_RES_2 = "자주 묻는 질문 답변 1";
+
+  static const String FAQ_3 = "자주 묻는 질문 3";
+  static const String FAQ_RES_3 = "자주 묻는 질문 답변 1";
+
+  static const String FAQ_4 = "자주 묻는 질문 4";
+  static const String FAQ_RES_4 = "자주 묻는 질문 답변 1";
+
+
+  // Device Status
+  /// Connection Fail
+  static const String CONN_FAIL = "연결 실패";
+  /// Connection Success
+  static const String CONN_SUCCESSED = "연결 성공";
+  /// Connection Error
+  static const String CONN_ERR = "연결 에러";
+
+}

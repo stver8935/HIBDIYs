37f468ec4bc8d4baccab1cdd303b4f475c866e39
LG <gihyeonghan@gmail.com>
2023-04-04 04:14:15 +0900

    refactor(공용 텍스트 수정): 상태값 설명 텍스트, 장비 설명 텍스트 추가
    상태값 설명 텍스트, 장비 설명 텍스트 추가

--- lib/res/Strings.dart
+++ lib/res/Strings.dart
@@ -27,7 +27,7 @@ class Strings{
   /** 장치 추가 */
   static const String ADD_DEVICE = "장치 추가";
   /** 장비 정보 상세 보기*/
-  static const String DEVICE_INFO_DETAIL = "장비정보 상세";
+  static const String DEVICE_INFO_DETAIL = "장비 상세 정보";
   /** 앱 설정 */
   static const String APP_SETTING = "앱 설정";
   /** 자주 묻는 질문 */
@@ -47,6 +47,13 @@ class Strings{
   /** 로그아웃 */
   static const String LOG_OUT = "로그아웃";
   static const String SALE_PRODUCT_MANAGE = "판매물품 관리";
+
+  static const String NAME = "이름";
+  static const String DEVICE_TYPE ="기기 타입";
+  static const String TEAM_VIEWER_ID = "팀뷰어 아이디";
+  static const String ADDRESS = "주소";
+  static const String CUR_UPDATE ="최근 업데이트";
+
 
 
   static const String YES = "예";
@@ -95,5 +102,23 @@ class Strings{
   static const String CONN_SUCCESSED = "연결 성공";
   /// Connection Error
   static const String CONN_ERR = "연결 에러";
+
+
+
+  /// explain text
+  /// 광량 상태값 설명
+  static const String EXP_STATE_LIGHT = "광량이 증가하면 광포화점까지 광헙성 작용이\n"
+      "왕성하여 생육량도 늘어나지만 작물에 따라 \n"
+      "차이가 있습니다.";
+  /// 온도 상태값 설명
+  static const String EXP_STATE_TMP = "식물의 탄소동화작용, 호흡작용, 증산작용 등\n"
+      " 생리적 작용에 영향을 미칩니다.";
+  /// 습도 상태값 설명
+  static const String EXP_STATE_HUM = "공기습도가 부족하면 잎 끝이 마르는 현상이 나타납니다.\n"
+      "반면에 공기 중의 습도가 너무 높으면\n"
+      " 증산 작용이 줄어들어 양분의 흡수와 \n"
+      "광합성 작용 또한 느려지며 꽃 피는 것도 방해를 받게 됩니다.";
+  /// 비옥도 상태값 설명
+  static const String EXP_STATE_FERT = "생육 환경내 식물이 필요로 하는 영양소를 수치화 한 값입니다.";
 
 }

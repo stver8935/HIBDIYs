0ff0e83dec4ab281692cb0fd878b3dccd7f1e284
LG <gihyeonghan@gmail.com>
2023-04-05 03:25:01 +0900

    feat(페이지 추가): 장비 상세 정보 페이지 추가
    장비 상세 정보 페이지 추가

--- lib/ui/screen/deviceinfodetail/DeviceInfoDetailPage.dart
+++ lib/ui/screen/deviceinfodetail/DeviceInfoDetailPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hibdiys/data/TestThemeData.dart';
+import 'package:hibdiys/res/FontSize.dart';
 import 'package:hibdiys/res/Strings.dart';
 import 'package:hibdiys/ui/custom/card/DeviceControllCard.dart';
 import 'package:hibdiys/ui/custom/card/DeviceInfoDetailCard.dart';
@@ -24,31 +25,49 @@ class _DeviceInfoDetailPage extends State<DeviceInfoDetailPage> implements Devic
   @override
   Widget build(BuildContext context) {
 
+    String selectedMenu = "";
+
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text(Strings.DEVICE_INFO_DETAIL),
+          backgroundColor: Colors.green,
+          title: const Text(Strings.DEVICE_INFO_DETAIL,style: TextStyle(fontWeight: FontWeight.bold,fontSize: FontSize.SIZE_25),),
           leading: IconButton(
-            icon : Icon(Icons.arrow_back),
+            icon : const Icon(Icons.arrow_back),
             onPressed: (){ Navigator.pop(context); },
           ),
           actions: [
-            IconButton(onPressed: (){print("share");}, icon: Icon(Icons.share)),
-            IconButton(onPressed: (){print("more");}, icon: Icon(Icons.more_vert))
+            PopupMenuButton<String>(
+              initialValue: selectedMenu,
+              // Callback that sets the selected popup menu item.
+              onSelected: (String item) {
+                setState(() {
+                  selectedMenu = item;
+                });
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                const PopupMenuItem<String>(
+                  value: "request",
+                  child: Text('해당 기기 문의'),
+                ),
+              ],
+            )
           ],
         ),
-      body:
-      Center(
+      body: Center(
         child: Container(
-            padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
             child: Column(
-                children: <Widget>[
-                  DeviceInfoDetailCard(item: TestThemeData.instance.getDeviceInfoDetail()),
-                  Expanded(child: DeviceStateCard(list:TestThemeData.instance.getDeviceStateList())),
-                  DeviceControllCard()
-                ]),
-              ),
-      ),
+              children: [
+                DeviceInfoDetailCard(item: TestThemeData.instance.getDeviceInfoDetail()),
+                DeviceStateCard(list:TestThemeData.instance.getDeviceStateList()),
+                const DeviceControllCard()
+              ],
+            ),
+          ),
+        )
+      )
     );
   }
 }

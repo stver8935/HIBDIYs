810c977fbb21462cbc0189735ef6173fbfb33233
LG <gihyeonghan@gmail.com>
2023-04-05 03:45:14 +0900

    feat(공통 클래스): 폰트 사이즈 클래스 추가
    폰트 사이즈 클래스 추가

---
+++ lib/res/FontSize.dart
@@ -0,0 +1,33 @@
+/// 어플리케이션 내에서 사용할 폰트 사이즈 모음 클래스
+///
+/// * 권장 폰트 사이즈
+///
+/// 본문 : 16pt ~ 18pt ( AOS : 16pt , IOS : 17pt )
+///
+/// 캡션 : 10pt ~ 12pt
+///
+/// 타이틀 : 18pt ~ 32pt
+class FontSize{
+  // 10pt 이하의 폰트 사이즈는 추가 하지 않을것을 권장
+  static const double SIZE_10 = 10.0;
+  static const double SIZE_11 = 11.0;
+  static const double SIZE_12 = 12.0;
+  static const double SIZE_16 = 16.0;
+  static const double SIZE_17 = 17.0;
+  static const double SIZE_18 = 18.0;
+  static const double SIZE_19 = 19.0;
+  static const double SIZE_20 = 20.0;
+  static const double SIZE_21 = 21.0;
+  static const double SIZE_22 = 22.0;
+  static const double SIZE_23 = 23.0;
+  static const double SIZE_24 = 24.0;
+  static const double SIZE_25 = 25.0;
+  static const double SIZE_26 = 26.0;
+  static const double SIZE_27 = 27.0;
+  static const double SIZE_28 = 28.0;
+  static const double SIZE_29 = 29.0;
+  static const double SIZE_30 = 30.0;
+  static const double SIZE_31 = 31.0;
+  static const double SIZE_32 = 32.0;
+
+}
